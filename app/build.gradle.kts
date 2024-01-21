2d5fa0522ac9cf93b8dd62e18a0d10ad4e7a5fd1
Ivan <smirnovivan228007@gmail.com>
2024-01-21 23:03:28 +0300

    change background color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dubby_app.myfirstgitprogect"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dubby_app.myfirstgitprogect"
